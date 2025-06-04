1774b019f16e8ee14840942c97a469a389eee7ac
Feno Radera <fenoradera@macbook-air.home>
2025-06-04 16:01:18 +0200

    add select box

--- app.py
+++ app.py
@@ -3,7 +3,18 @@ import streamlit as st
 import pandas as pd
 import duckdb
 
-st.write("hello world")
+st.write("""
+# SQL SRS
+Spaced Repetition System SQL practice
+""")
+
+option = st.selectbox(
+    "What would you like to review?",
+    ("Joins", "GroupBy", "Window Functions"),
+    index=None,
+    placeholder="Select a theme...",
+)
+
 data = {'a':[1, 2, 3], "b":[4, 5, 6]}
 df = pd.DataFrame(data)
 

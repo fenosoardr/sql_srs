cb52abbac6a1a38a53171f47d6ce7410f2f7c61c
Feno Radera <fenoradera@macbook-air.home>
2025-06-04 17:43:16 +0200

    modify app and add check code

--- app.py
+++ app.py
@@ -1,53 +1,60 @@
-import pip
-import streamlit as st
-import pandas as pd
-import duckdb
+#pylint: disable=missing-module-docstring
 import io
 
+import duckdb
+import pandas as pd
+import streamlit as st
 
-csv = '''
+CSV = """
 beverage,price
 orange juice,2.5
 Expresso,2
 Tea,3
-'''
-beverages = pd.read_csv(io.StringIO(csv))
+"""
+beverages = pd.read_csv(io.StringIO(CSV))
 
-csv2 = '''
+CSV2 = """
 food_item,food_price
 cookie juice,2.5
 chocolatine,2
 muffin,3
-'''
-food_items = pd.read_csv(io.StringIO(csv2))
+"""
+food_items = pd.read_csv(io.StringIO(CSV2))
 
-answer = """
+ANSWER_STR = """
 SELECT * FROM beverages CROSS JOIN food_items
 """
-solution = duckdb.sql(answer).df()
-
-st.write("""
-# SQL SRS
-Spaced Repetition System SQL practice
-""")
+solution_df = duckdb.sql(ANSWER_STR).df()
 
 st.header("enter your code :")
-query = st.text_area("votre Code SQL ici",  key = 'user_input')
-if query :
+query = st.text_area("votre Code SQL ici", key="user_input")
+if query:
     result = duckdb.sql(query).df()
     st.dataframe(result)
 
-with st.sidebar :
+    try:
+        result = result[solution_df.columns]
+        st.dataframe(result.compare(solution_df))
+    except KeyError as e:
+        st.write("Some columns are missing")
+
+    n_lines_difference = result.shape[0] - solution_df.shape[0]
+    if n_lines_difference != 0:
+        st.write(
+            f"The results has a {n_lines_difference} lines difference with the solution"
+        )
+
+
+with st.sidebar:
     option = st.selectbox(
         "What would you like to review?",
         ("Joins", "GroupBy", "Window Functions"),
         index=None,
         placeholder="Select a theme...",
     )
-    st.write('You selected:', option)
+    st.write("You selected:", option)
 
-tab2, tab3 = st.tabs([ "Tables", "Solutions"])
-
+tab2, tab3 = st.tabs(["Tables", "Solutions"])
 
 
 with tab2:
@@ -56,7 +63,7 @@ with tab2:
     st.write("table : food_items")
     st.dataframe(food_items)
     st.write("expected :")
-    st.dataframe(solution)
+    st.dataframe(solution_df)
 
 with tab3:
-    st.write(answer)
+    st.write(ANSWER_STR)
